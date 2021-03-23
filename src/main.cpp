5fd2cffd9b294741b5fc8bda792958da6def8943
acse-yl1720 <72428620+acse-yl1720@users.noreply.github.com>
2021-03-23 21:49:32 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,5 @@
 using namespace std;
 int main(int argc, char * argv[])
 {
-    double feed_gor_rate = 10;
-    double feed_waste_rate = 100;
-
-    //set the feed to the cell receiving the curcuit feed equal to the flowrates of the curcuit feed
-    CUnit first_unit = units[0];
-    first_unit.feed_gor_rate = feed_gor_rate;
-    first_unit.feed_waste_rate = feed_waste_rate;
-    
-    //Start with the vectors representing the initial random collection of valid circuits.
-    
-    for(int i = 0; i < num_units; i++){
-        
-    }
     return 0;
 }
