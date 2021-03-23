cf21260c0bab15029f606128d5e8b44191fd0b10
acse-yl1720 <72428620+acse-yl1720@users.noreply.github.com>
2021-03-23 18:55:42 +0800

    Update Genetic_Algorithm.cpp

--- src/Genetic_Algorithm.cpp
+++ src/Genetic_Algorithm.cpp
@@ -4,18 +4,20 @@
 #include "CUnit.h"
 #include "CCircuit.h"
 #include "Genetic_Algorithm.h"
+#include "CCircuit.cpp"
 
 int answer_vector[2*num_units+1] = {0, 1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 8, 9,
-				10, 11, 10, 11, 10, 11, 10, 11}  ;
+                10, 11, 10, 11, 10, 11, 10, 11}  ;
 
 double Evaluate_Circuit(int *circuit_vector, double tolerance, int max_iterations) {
-
-  int vector_size = 2*num_units+1;
-
-  double Performance = 0.0; 
-  for (int i=0;i<vector_size;i++) {
-    //answer_vector is a predetermined answer vector (same size as circuit_vector)
-    Performance += (20-std::abs(circuit_vector[i]-answer_vector[i]))*100.0; 
-  }
-  return Performance; 
+    int vector_size = 2*num_units+1;
+    int concentrate_num = num_units;
+    double Performance = 0.0;
+    
+    for (int i = 0;i < vector_size; i++) {
+        if(circuit_vector[i] == concentrate_num){
+            Performance += (units[i].conc_gor_rate*100 - units[i].conc_waste_rate*500);
+        }
+    }
+    return Performance;
 }
