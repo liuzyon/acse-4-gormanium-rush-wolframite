66b2eb81893e55250d45aa9998e39fc4e2bcf562
acse-yl1720 <72428620+acse-yl1720@users.noreply.github.com>
2021-03-23 23:06:46 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <cmath>
 #include "CUnit.h"
 #include "CCircuit.h"
-#include "CCircuit.cpp"
+#include "Genetic_Algorithm.h"
 
 using namespace std;
 int main(int argc, char * argv[])
 {
+    cout << "Starting genetic algorithm program..." << endl;
+    int circuit_vector[11] = {0,4,3,2,0,5,4,4,6,2,1};
+    int max_iterations = 243;
+    double tolerance = 1.0e-6;
+    double performance = Evaluate_Circuit(&circuit_vector[0], tolerance, max_iterations);
+    cout << "Performance: " <<  performance << endl;
     return 0;
 }
