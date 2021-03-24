177aa20e869a028a4a099e72d22cf4d95d153f23
acse-yl1720 <yinglinim@gmail.com>
2021-03-24 16:56:44 +0800

    add test

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -21,7 +21,7 @@ int main(int argc, char * argv[])
     int vec2[2*num_units+1] = {6,16,7,7,13,11,12,15,5,3,6,0,2,14,12,1,12,14,11,5,16,11,9,4,1,0,8,5,10,2,6};
     max_iterations = 10000;
     std::cout << "Evaluate_Circuit(vec2, " << tolerance << ", " << max_iterations << ") close to 10.47:\n";
-    if (std::fabs(Evaluate_Circuit(vec2, tolerance, max_iterations) - 10.47) < tolerance))
+    if (std::fabs(Evaluate_Circuit(vec2, tolerance, max_iterations) - 10.47) < tolerance)
         std::cout << "pass\n";
     else
         std::cout << "fail";
