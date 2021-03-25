7afb98b2fcc36c521af68e962134255191b3d1a8
acse-tz3420 <tz3420@ic.ac.uk>
2021-03-25 21:18:50 +0800

    Create Post_Processing.py

---
+++ Post_Processing.py
@@ -0,0 +1,64 @@
+import graphviz
+
+
+def visualization(unit_num, vector, vector_string, iteration, performance):
+    """
+    This function can draw the citcuit connection graph with input information.
+    The graph will be generate in png format in output folder.
+    """
+    vec = graphviz.Digraph('Circuit connections')
+
+    vec.attr(rankdir='LR', ranksep="0.3 equally")
+    vec.attr('node', shape='box', size='0.02')
+
+    # Draw feed cluster
+    with vec.subgraph(name='cluster_feed') as c:
+        c.attr(compound='false', color='#ffffff')
+        c.node('feed')
+
+    # Draw results cluster
+    with vec.subgraph(name='cluster_results') as c:
+        c.attr(compound='false', color='#ffffff')
+        c.node('%d' % unit_num, 'concentrate')
+        c.node('%d' % (unit_num + 1), 'tailings')
+
+    # Draw connection between feed and the first unit
+    vec.edge('feed', '%d' % vector[0])
+
+    # Draw connection between all units and concentrate/tailings
+    for i in range(unit_num):
+        vec.edge(
+            '%d:n' % i, '%d:w' % vector[2 * i + 1],
+            color="#0000ff", splines='curved'
+            )
+        vec.edge(
+            '%d:s' % i, '%d:w' % vector[2 * i + 2],
+            color="#ff0000", splines='curved'
+            )
+
+    # Set graph information
+    vec.attr(
+        label=r'\n\nIteration: %-10s Performance: %-10s vector: %s'
+        % (iteration, performance, vector_string)
+        )
+    vec.attr(fontsize='14')
+
+    # Write to disk
+    vec.render(
+        filename='circuit', directory='output', cleanup=True, format='png'
+        )
+
+
+if __name__ == "__main__":
+    # Read in information
+    with open("output/output.txt", "r") as f:
+        data = f.readlines()
+
+    vector_string = data[0].strip('\n')
+    iteration = data[1].strip('\n')
+    performance = data[2].strip('\n')
+
+    vector = list(map(int, vector_string.split()))
+    unit_num = int((len(vector) - 1) / 2)
+
+    visualization(unit_num, vector, vector_string, iteration, performance)
