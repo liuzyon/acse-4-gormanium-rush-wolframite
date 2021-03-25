a969d56966f19f3cf2ad570e6d889c9c7abd35dd
acse-yd1820 <yuyan.ding20@imperial.ac.uk>
2021-03-25 20:03:50 +0800

    check the validity

--- includes/Genetic_Algorithm.h
+++ includes/Genetic_Algorithm.h
@@ -1,157 +1,42 @@
 #pragma once
 #include <vector>
-//Hyper parameters
-/// The number of the solutions, ie population
-static int POP_SIZE = 100;
-/// The rate of cross over. The range is from 0 to 1
-static double CROSSOVER_RATE = 0.8;
-/// The rate of mutation. The range is from 0 to 1
-static double MUTATION_RATE = 0.1;
-/// The number of iteration. It must more than 0
-static int MAX_ITERATION = 100;
+#include <stdio.h>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include "CUnit.h"
+#include "CCircuit.h"
+#include <omp.h>
 
-/**
-* @brief This is a class that represent a chromosome.
-* The chromosome has curit_vector representing the gene sequence.
-* 
-*/
-// Solution class, store the vector and fitness value
+//Hyper parameters
+static int POP_SIZE = 150;
+static double CROSSOVER_RATE = 0.8;
+static double MUTATION_RATE = 0.001;
+static int MAX_ITERATION = 1000;
+
+//Solution class, store the vector and fitness value
 class solution {
 public:
-	int* curit_vector;   ///< It is a address of array, which contains the gene sequence of this chromosome
-	/// The fitness value of the vector
-	/// @see Evaluate_Circuit()
+	int* curit_vector;
 	double fitness_value;
 };
 
-/**
-        * @brief		Print the value in the vector
-        * @param	    vec : the address of the array,
-        * @return       void
-        * @par Sample
-        * @code
-        *	int vec[2 * num_units + 1] = { 0,4,3,2,0,5,4,4,6,2,1 };
-        *	print_vector(int* vec);
-        * @endcode
-        */
+bool same_vector(int* v1, int* v2);
+
 void print_vector(int* vec);
 
-/**
-        * @brief		This is a fitness function.
-        * @param	    circuit_vector : the address of the array,
-                        which contains the curit_vector of this solution.
-        * @param	    tolerance :
-        * @param	    max_iterations : the number of the maximum iteration.
-        * @return       The double type value \n
-        * @note	        The minimum of this value of this function -50000, 
-        *               when the circuit is not steady.
-        * @attention    max_iteration must more than 0.
-        * @details  In this project, Each unit will recover 20% of 
-        *           the gormanium to the concentrate together with 5% of 
-        *           the waste. We will be paid £100 per kg of gormanium 
-        *           in the product and charged £500 per kg of the waste material.
-        *           According to these rule, we caulculate the final profit using
-        *           this function.
-        * @par Sample
-        * @code
-        *	int vec[2 * num_units + 1] = { 0,4,3,2,0,5,4,4,6,2,1 };
-        *	double x = Evaluate_Circuit(vec , 1e-6 , 1000);
-        * 	std::cout << x <<std::endl;
-        * @endcode
-        */
-double Evaluate_Circuit(int* circuit_vector, double tolerance, int max_iterations);
+double Evaluate_Circuit(int *circuit_vector, int num_units, double tolerance, int max_iterations);
 
-/**
-        * @brief		This is a function that initialise the population
-        * @param	    pop : the address of the array, which contains the solutions
-        * @return       void
-        * @details      This function is used to initialise the POP_SIZE solutions that
-        *               meet the requirements. The function that check the solutions' 
-        *               validty is in function Check_Validity(int* circuit_vector). \n
-        */
 void init_population(solution* pop);
 
-
-/**
-        * @brief		This is a function that free the heap memory of each solution in population
-        * @param	    pop : the address of the array, which contains the solutions
-        * @return       void
-        */
 void free_memory(solution* pop);
 
-/**
-        * @brief		This is a function that randomly generated double value from 0 to 1
-        * @param	    void
-        * @return       The value of double type
-        * @par Sample
-        * @code
-        *	double x =  rand_01();
-        * 	std::cout << x <<std::endl;
-        * @endcode
-        */
 double rand_01();
 
-/**
-        * @brief		This is a function that select one solution from the population
-        * @param	    pop : the address of the array, which contains the solutions
-        * @return       The value of int type
-        * @details      We select the elements in the population according to the roulette method.
-        */
 int selection(solution* pop);
 
-/**
-        * @brief		This is a function that makes two solutions crossover to 
-        *               generate two new solutions.
-        * @param	    curt_pop : the address of the array,which contains the solutions
-        * @param	    child_1 : An object of the solution type 
-        * @param	    child_2 : An object of the solution type 
-        * @return       void
-        * @details      Using the selection(solution* pop) function to choose two solutions as parents 
-        *               in population.Randomly decide if the parents should crossover. In this project, 
-        *               we set the rate of the crossover is 0.8. You can change the rate by by yourself.
-        *               If they don’t cross, they both go to the next step unchanged. If they are to 
-        *               cross,  a random point in the vector is chosen and all of the values before 
-        *               that point are swapped with the corresponding points in the other vector.After 
-        *               cross over, store the two curit_vector in child_1 and child_2 respectively.
-        */
 void cross_over(solution* curt_pop, solution& child_1, solution& child_2);
 
-/**
-        * @brief		This is a function that makes the solution mutate.
-        * @param	    curt_sol : An object of the solution type
-        * @return       void
-        * @details      Go over each of the numbers in the vectors and decide whether to mutate the curit_vector
-        *               of this solution. You can set the rate of mutate through changing the double MUTATION_RATE.
-        *               Randomly generate a number in the range from 0 to (num_units+1) as the value in this index 
-        *               after mutation.
-        */
 void mutation(solution& curt_sol);
 
-
-/**
-        * @brief		This is a function that execute the genetic algorithm.
-        * @param	    void
-        * @return       void
-        * @details      The steps of genetic algorithm are initialization, selection,cross over and mutation. 
-        *               It will replace the parent vectors with generated child vectors and repeat the process
-        *               until a set number of iterations have been completed or a threshold has been met.
-        */
 void Genetic_algorithm();
-
-/**
-        * @brief		This is a function that get the index of the solution in population that has the maximume 
-        *               performance value.
-        * @param	    pop : the address of the array,which contains the solutions
-        * @return       The value of int type
-        */
-int find_best_sol(solution* pop);
-
-
-/**
-        * @brief		This is a function that copy the value of sol1's curit_vector to sol2's.
-        *               This is deep copy.
-        * @param	    sol1 : An object of the solution type
-        * @param	    sol2 : An object of the solution type
-        * @return       void
-        */
-void copy_solution(solution& sol1, solution& sol2);
--- src/CCircuit.cpp
+++ src/CCircuit.cpp
@@ -7,39 +7,76 @@
 
 using namespace std;
 
+static bool valid;
+static bool debug = true;
+
 std::vector<CUnit> units(num_units);
 
-bool Check_Validity(int *circuit_vector)
+//assign conc_num&tails_num to units[unit_index]
+void assignUnit(int *circuit_vector, int unit_index, int &vector_index)
 {
-  return true;
-}
-
-void mark_units(int unit_num) {
-
-  if (units[unit_num].mark) return;
-
-  units[unit_num].mark = true;
-
-  //If we have seen this unit already exit
-  //Mark that we have now seen the unit
-  //If conc_num does not point at a circuit outlet recursively call the function
-
-  if (units[unit_num].conc_num<num_units) {
-    mark_units(units[unit_num].conc_num);
-  } else {
-    // ...Potentially do something to indicate that you have seen an exit
-  }
-  //If tails_num does not point at a circuit outlet recursively call the function
-
-  if (units[unit_num].tails_num<num_units) {
-    mark_units(units[unit_num].tails_num);
-  } else {
-    // ...Potentially do something to indicate that you have seen an exit
-  }
+    if(unit_index < num_units)
+    {
+        units[unit_index].conc_num = circuit_vector[vector_index];
+        units[unit_index].tails_num = circuit_vector[vector_index + 1];
+        vector_index += 2;
+        assignUnit(circuit_vector, unit_index + 1, vector_index);
+    }
+    return;
 }
 
 
-void dfs(int start, int *vis, vector<vector<int> > &inverse_graph)
+void mark_units(int unit_num)
+{
+    if(valid == false || units[unit_num].mark)
+    {
+        return;
+    }
+
+    units[unit_num].mark = true;
+
+    //detect self recyle
+    if(units[unit_num].conc_num == unit_num || units[unit_num].tails_num == unit_num)
+    {
+        valid = false;
+        return;
+    }
+
+    //detect the destinations are the same unit
+    if(units[unit_num].conc_num == units[unit_num].tails_num)
+    {
+        valid = false;
+        return;
+    }
+
+    //If we have seen this unit already exit
+    //Mark that we have now seen the unit
+    //If conc_num does not point at a circuit outlet recursively call the function
+    if (units[unit_num].conc_num<num_units) 
+    {
+        mark_units(units[unit_num].conc_num);
+    }
+    else
+    {
+        // ...Potentially do something to indicate that you have seen an exit
+        units[unit_num].reachConcentrate = true;
+    }
+
+
+    //If tails_num does not point at a circuit outlet recursively call the function
+    if (units[unit_num].tails_num<num_units)
+    {
+        mark_units(units[unit_num].tails_num);
+    }
+    else
+    {
+        // ...Potentially do something to indicate that you have seen an exit
+        units[unit_num].reachTailings = true;
+    }
+}
+
+
+void dfs(int start, int *vis, vector< vector<int> > &inverse_graph)
 {
   if (vis[start] == 1) return; // if seen before, stop search
 
@@ -69,29 +106,67 @@ bool check_requ_two(int *circuit_vector)
   int vis_con[num_units+2]={0};
   dfs(num_units, vis_con, inverse_graph);
 
-  // for (int i = 0; i < num_units+2; i++)
-  // {
-  //   cout << vis_con[i] << " ";
-  // }
   
   int test_con = accumulate(vis_con, vis_con+num_units+2, 0);
   // cout << "test final concentrate: " << test1 << endl;
   bool con_pass = (test_con == num_units+1) && (vis_con[num_units+1] == 0);
   
-  // cout << "Test for each cell to final talings" << endl;
   //test for each cell to final tailings
   int vis_tail[num_units+2]={0};
   dfs(num_units+1, vis_tail, inverse_graph);
 
-  // for (int i = 0; i < num_units+2; i++)
-  // {
-  //   cout << vis_tail[i] << " ";
-  // }
+  
   
   int test_tail = accumulate(vis_tail, vis_tail+num_units+2, 0);
   // cout  <<  "test final tailings: " << test2 << endl;
   bool tail_pass = (test_tail == num_units+1) && (vis_tail[num_units] == 0);
+  
+  valid = tail_pass&&con_pass;
 
+  return valid;
+}
 
-  return con_pass && tail_pass;
+bool Check_Validity(int *circuit_vector)
+{
+    valid = true;
+    int startUnit = circuit_vector[0];
+    //build the connection between units
+    int vector_index = 1;
+    assignUnit(circuit_vector, 0, vector_index);
+    
+    /*
+    std::cout<<"circuit_vector="<<std::endl;
+    for(int i = 0; i < 2 * num_units + 1; ++i)
+    {
+        std::cout<<circuit_vector[i]<<" ";
+    }
+    std::cout<<std::endl;
+    std::cout<<"units="<<std::endl;
+    for(int i = 0; i < num_units; ++i)
+    {
+        std::cout<<i<<" con="<<units[i].conc_num<<" tail="<<units[i].tails_num<<std::endl;
+    }
+    */
+
+    for(int i = 0; i < num_units; ++i)
+    {
+        units[i].mark = units[i].reachConcentrate = units[i].reachTailings = false;
+    }
+    //traverse start at 0
+    mark_units(startUnit);
+
+    
+    //check whether all unit are accessible
+    for(int i = 0; i < num_units; ++i)
+    {
+        //this unit is not available
+        if(units[i].mark == false)
+        {
+            return false;
+        }
+    }
+    
+    check_requ_two(circuit_vector);
+
+    return valid;
 }
