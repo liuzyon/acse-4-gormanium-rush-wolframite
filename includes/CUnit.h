381dd76b479d3abf61b95bfe8adeace0ace62c2c
nirvana <nirvana.liu@outlook.com>
2021-03-25 17:10:07 +0800

    update for doc in CUnit.h

--- includes/CUnit.h
+++ includes/CUnit.h
@@ -1,6 +1,6 @@
 /**                                 
  * @file CUnit.h                      
- * @author liuzhiyong
+ * @author LIU ZHIYONG
  * @version 1.0
  * @date 2021.3.24
  * @brief The header file of CUnit
@@ -24,9 +24,9 @@ private:
 
 public:
 
-    int conc_num;   ///< index of the unit to which this unit’s concentrate stream is connected
+    int conc_num;   ///< Index of the unit to which this unit’s concentrate stream is connected
 
-    int tails_num;  ///< index of the unit to which this unit’s concentrate stream is connected
+    int tails_num;  ///< Index of the unit to which this unit’s concentrate stream is connected
 
     bool mark;  ///< A Boolean that is changed to true if the unit has been seen
 
@@ -36,52 +36,71 @@ public:
 
     double tails_waste_rate;    ///< the waste rate of tail stream in this unit
 
-    ///< the valuable rate of tail stream in this unit
-    double tails_gor_rate;
+    double tails_gor_rate;  ///< the valuable rate of tail stream in this unit
 
-    ///< the valuable material rate of feed in this unit
-    double feed_gor_rate;
+    double feed_gor_rate;   ///< the valuable material rate of feed in this unit
 
     double feed_waste_rate; ///< the waste rate of feed in this unit
 
     double old_feed_rate;   ///< old feed rate in this unit
 
     /**
-     * @brief Set the id of unit
-     * @param id an integer argument
+     * @brief This is a function that set the id of unit
+     * @param id : an integer argument
+     * @return void
      */
     void setId(int id);
 
     /**
      * @brief Init the feed of unit
+     * @return void
+     * @details This function is used to initialise the feed of each unit.
+     *          The initial feed of unit is set to the value of whole circuit
+     *          feed.
      */ 
     void init();
 
     /**
-     * @brief Calculate the flowrate of each component in both the concentrate and tailings streams 
+     * @brief Calculate the flowrate of each component in concentrate and tailings streams
+     * @return void
+     * @details use the current guess of the feed flowrates to calculate 
+     *          the flowrate of each component in both the concentrate and 
+     *          tailings streams. Each unit will recover 20% of the valuable 
+     *          material to the concentrate together with 5% of the waste.
      */ 
     void cal_con_tail();
 
     /**
-     * @brief Store the current value of the feed to this cell as an old feed value and set the current value for all components to zero
+     * @brief This function is used to record the current value and reset it
+     * @return void
+     * @details Store the current value of the feed to this cell as an old feed value 
+*               and set the current value for all components to zero
      */
     void reset();
 
     /**
-     * @brief Add the concentrate flows to the appropriate unit’s feed based on the linkages in the circuit vector.
-     * @param units an reference of vector
+     * @brief This function is used to transfer the concentrate flow
+     * @param units : an reference of vector
+     * @return void
+     * @details Add the concentrate flows to the appropriate unit’s feed 
+     *          based on the linkages in the circuit vector.
      */
     void transf_con(std::vector<CUnit> &units);
 
     /**
-     * @brief Add the tailings flows to the appropriate unit’s feed based on the linkages in the circuit vector.
+     * @brief This function is used to transfer the tailing flow
      * @param units an reference of vector
+     * @return void
+     * @details Add the tailings flows to the appropriate unit’s feed 
+     *          based on the linkages in the circuit vector.
      */
     void transf_tail(std::vector<CUnit> &units);
 
     /**
-     * @brief Check the difference between the newly calculated feed rate and the old feed rate for this cell
-     * @return the absoluate value of difference
+     * @brief This function is used to compare new feed with old feed
+     * @return double the absoluate value of difference
+     * @details Check the difference between the newly calculated 
+     *          feed rate and the old feed rate for this cell
      */
     double compare_to_old();
 
