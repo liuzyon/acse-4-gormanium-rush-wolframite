7da5dac9a0d380fd6ea19f959f49fded46bbd8ed
acse-yl1720 <yinglinim@gmail.com>
2021-03-26 14:58:56 +0800

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include "Genetic_Algorithm.h"
-
+#include "CCircuit.h"
 
 int main(int argc, char * argv[])
 {
+    cout << "Starting The Gormanium Rush..." << endl;
+    cout << "Gormanium feed rate: " << circuit_gor << "kg/s" << endl;
+    cout << "Waste feed rate: " << circuit_waste << "kg/s" << endl;
+    cout << "purity of feed stream: " << purity_feed << "%" << endl;
+    cout << "Gormanium price: " << gor_price << "£/kg" << endl;
+    cout << "Waste cost: " << waste_price << "£/kg" << endl;
+    cout << "the number of units in a circuit: " << num_units << endl;
     Genetic_algorithm();
     return 0;
 }
